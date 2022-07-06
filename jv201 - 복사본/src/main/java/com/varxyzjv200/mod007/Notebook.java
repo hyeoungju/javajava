5a58376768dd711e8db3a5824cc827d442224887
hyeoungju <nhj0710@naver.com>
2022-07-06 17:19:25 +0900

    aa
    aa

---
+++ jv201 - 복사본/src/main/java/com/varxyzjv200/mod007/Notebook.java
@@ -0,0 +1,12 @@
+package com.varxyzjv200.mod007;
+
+public class Notebook extends Product {
+	public Notebook(String name) {
+		super(name);
+	}
+	
+	public double getPrice() {
+		return 25000.0;
+				
+	}
+}
